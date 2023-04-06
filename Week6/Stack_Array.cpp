2fb1fc17b921369eca0da88c87815ce7d45f1871
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2023-04-06 14:28:20 +0530

    Array implementation of stack

---
+++ Week6/Stack_Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int stack[100], n=100, top=-1;
+
+
+void push(int val) {
+   if(top>=n-1)
+   cout<<"Stack Overflow"<<endl;
+   else {
+      top++;
+      stack[top]=val;
+   }
+}
+void pop() {
+   if(top<=-1)
+   cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< stack[top] <<endl;
+      top--;
+   }
+}
+
+int main() {
+   
+   
+   return 0;
+}
