962617d5a8c938a9607ef2b2308e2640dfe339ab
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2023-04-27 14:33:46 +0530

    insert and traverse in order done

---
+++ Week7/lab7BST.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+  int key;
+  struct node *left, *right;
+};
+
+// Inorder traversal
+void traverseInOrder(struct node *root) {
+  
+    if(root->left != NULL){
+        traverseInOrder(root->left);
+    }
+    cout<<" "<<root->key;
+    if(root->right != NULL){
+        traverseInOrder(root->right);
+    }
+    
+}
+
+// Insert a node
+struct node *insertNode(struct node *node, int key) {
+
+
+    
+    if(node == NULL){
+        struct node *nodeFirst = new struct node;
+        nodeFirst->key = key;
+        return nodeFirst;
+    }
+
+    if(node->key > key){
+        node->left = insertNode(node->left,key);
+    }
+    else{
+        node->right = insertNode(node->right,key);
+    }
+
+    return node;
+
+}
+
+// Deleting a node
+struct node *deleteNode(struct node *root, int key) {
+ 
+}
+
+// Driver code
+int main() {
+  struct node *root = NULL;
+
+  int operation;
+  int operand;
+  cin >> operation;
+
+  while (operation != -1) {
+    switch(operation) {
+      case 1: // insert
+        cin >> operand;
+        root = insertNode(root, operand);
+        cin >> operation;
+        break;
+      case 2: // delete
+        cin >> operand;
+        root = deleteNode(root, operand);
+        cin >> operation;
+        break;
+      default:
+        cout << "Invalid Operator!\n";
+        return 0;
+    }
+  }
+  
+  traverseInOrder(root);
+}
