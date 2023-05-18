62c869df04db6ae9d2cb44eac7177978d2d274be
Sanjula Gathsara <111214734+sanjulagathsarauom@users.noreply.github.com>
2023-05-18 16:15:47 +0530

    Done

---
+++ Week10/graph.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+struct Node{
+    int label;
+    list<int>  neighbours;
+};
+
+struct Graph{
+
+    int n=8;
+    Node * nodes = new Node[n];
+
+    void intializenodes(){
+        //iteratION THROUGH NODES
+        for (int i = 0; i < n; i++)
+            nodes[i].label = i + 1;
+
+    }
+
+    void addedge(int u, int v){
+        nodes[u-1].neighbours.push_back(v);
+        nodes[v-1].neighbours.push_back(u);
+    }
+
+    void print(){
+        for (int i=0;i<n;i++){
+            cout<<nodes[i].label<<" --> ";
+            for (int neighbour:nodes[i].neighbours){
+                cout<<neighbour<<" ";
+            }
+            cout<<endl;
+        }
+
+    }
+};
+
+int main() {
+    Graph * g = new Graph;
+    g->intializenodes();
+    //add edges for the graphs here.
+
+    g->addedge(1,2);
+    g->addedge(1,3);
+    g->addedge(1,4);
+    g->addedge(1,5);
+    g->addedge(2,3);
+    g->addedge(2,6);
+    g->addedge(4,6);
+    g->addedge(4,7);
+    g->addedge(4,8);
+    g->addedge(5,6);
+    g->addedge(5,7);
+    g->addedge(5,8);
+
+    //print the graph adjaceny list
+    g->print();
+}
